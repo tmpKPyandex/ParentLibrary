fa5b7464766eaafa6a3d98d03bde22d066f72076
Christian Benua <chris-benua@yandex-team.ru>
2023-04-12 14:20:26 +0300

    Initial Commit

---
+++ Tests/ParentLibraryTests/ParentLibraryTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ParentLibrary
+
+final class ParentLibraryTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ParentLibrary().text, "Hello, World!")
+    }
+}
