37413b85cf1f2766852a50fd06588656520ac780
Christian Benua <chris-benua@yandex-team.ru>
2023-04-12 14:25:43 +0300

    Added ChildLibrary Dependency

--- Package.swift
+++ Package.swift
@@ -12,15 +12,14 @@ let package = Package(
             targets: ["ParentLibrary"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/tmpKPyandex/ChildLibrary", exact: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ParentLibrary",
-            dependencies: []),
+            dependencies: [.product(name: "ChildLibrary", package: "ChildLibrary")]),
         .testTarget(
             name: "ParentLibraryTests",
             dependencies: ["ParentLibrary"]),
--- Sources/ParentLibrary/ParentLibrary.swift
+++ Sources/ParentLibrary/ParentLibrary.swift
@@ -1,3 +1,5 @@
+import ChildLibrary
+
 public struct ParentLibrary {
     public private(set) var text = "Hello, World!"
 
